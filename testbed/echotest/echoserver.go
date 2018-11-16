8fa5e150c38734ff0358565cc7babc08f6ef77c8
neo3child <live1child@gmail.com>
2018-11-17 05:48:42 +0900

    M0 First Upload

---
+++ testbed/echotest/echoserver.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"net/http"
+)
+
+func main() {
+
+	e := echo.New()
+
+	e.POST("/users", saveUser)
+	e.GET("/users/:id", getUser)
+	e.PUT("/users/:id", updateUser)
+	e.DELETE("/users/:id", deleteUser)
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":8080"))
+}
